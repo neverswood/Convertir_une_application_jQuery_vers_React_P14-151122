407f790f38cfdae43c1e0cc113d17b7e22137cb7
neverswood <nevers_wood@hotmail.fr>
2023-01-24 21:37:10 +0100

    Change the CreateEmployee component to retrieve dropdown options

--- src/page/HomePage/CreateEmployee.tsx
+++ src/page/HomePage/CreateEmployee.tsx
@@ -19,7 +19,7 @@ export function CreateEmployee() {
   const [city, setCity] = useState('');
   const [zipCode, setZipCode] = useState('');
   const [department, setDepartment] = useState('');
-  const [state, setState] = useState('');
+  const [state, setState] = useState({ name: '', value: '' });
   const [showModal, setShowModal] = useState(false);
   const [error, setError] = useState<Map<string, string>>(new Map());
   const hideModal = () => showModal && setShowModal(false);
@@ -34,7 +34,7 @@ export function CreateEmployee() {
     street: street,
     city: city,
     zip_code: zipCode,
-    state: state,
+    state: state.value,
   };
 
   const onSubmit = (e: React.FormEvent) => {
@@ -61,7 +61,7 @@ export function CreateEmployee() {
     } else {
       newError.delete('startDate');
     }
-    if (state === '') {
+    if (state.name === '') {
       newError.set('state', 'error state');
     } else {
       newError.delete('state');
@@ -97,174 +97,181 @@ export function CreateEmployee() {
   return (
     <React.Fragment>
       <form onSubmit={onSubmit}>
-        <fieldset className="create-employee__fieldset">
+        <fieldset className="create-employee">
           <legend className="create-employee__legend">Create Employee</legend>
-          <div className="information">
-            <Input
-              inputName="input-wrapper"
-              labelFor="firstName"
-              labelText="First Name"
-              type="text"
-              id="firstName"
-              placeholder=" Enter your first name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            {error.has('firstName') === true ? (
-              <div className="errorMessage">
-                Please enter at least 2 characters
-              </div>
-            ) : (
-              ''
-            )}
-
-            <Input
-              inputName="input-wrapper"
-              labelFor="lastName"
-              labelText="Last Name"
-              type="text"
-              id="lastName"
-              placeholder=" Enter your lastName"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            {error.has('lastName') ? (
-              <div className="errorMessage">
-                Please enter at least 2 characters
-              </div>
-            ) : (
-              ''
-            )}
-
-            <div className="calendar">
-              <label htmlFor="date" className="date">
-                Date Of Birth
-              </label>
-              <DatePicker
-                onChange={setDateOfBirth}
-                value={dateOfBirth}
-                clearIcon={null}
-                format="dd-MM-y"
-                className="calendar-popper"
-                required={true}
-                maxDate={
-                  new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 6574)
-                }
+          <div className="create-employee__form">
+            <div className="information">
+              <Input
+                inputName="input-wrapper"
+                labelFor="firstName"
+                labelText="First Name"
+                type="text"
+                id="firstName"
+                placeholder=" Enter your first name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
               />
-              {error.has('dateOfBirth') ? (
-                <div className="errorMessage">Please enter a valid date</div>
+              {error.has('firstName') === true ? (
+                <div className="errorMessage">
+                  Please enter at least 2 characters
+                </div>
               ) : (
                 ''
               )}
-            </div>
-            <div className="calendar">
-              <label htmlFor="date" className="date">
-                Start Date
-              </label>
-              <DatePicker
-                onChange={setStartDate}
-                value={startDate}
-                clearIcon={null}
-                format=" dd-MM-y "
-                maxDate={new Date()}
+
+              <Input
+                inputName="input-wrapper"
+                labelFor="lastName"
+                labelText="Last Name"
+                type="text"
+                id="lastName"
+                placeholder=" Enter your lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
               />
-              {error.has('startDate') ? (
-                <div className="errorMessage">Please enter a valid date</div>
+              {error.has('lastName') ? (
+                <div className="errorMessage">
+                  Please enter at least 2 characters
+                </div>
               ) : (
                 ''
               )}
-            </div>
-          </div>
-          <div className="adress">
-            <fieldset className="adress__fieldset">
-              <legend className="adress__legend">Adress</legend>
+
+              <div className="calendar">
+                <label htmlFor="date" className="date">
+                  Date Of Birth
+                </label>
+                <DatePicker
+                  onChange={setDateOfBirth}
+                  value={dateOfBirth}
+                  clearIcon={null}
+                  format="dd-MM-y"
+                  className="calendar-popper"
+                  required={true}
+                  maxDate={
+                    new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 6574)
+                  }
+                />
+                {error.has('dateOfBirth') ? (
+                  <div className="errorMessage">Please enter a valid date</div>
+                ) : (
+                  ''
+                )}
+              </div>
+              <div className="calendar">
+                <label htmlFor="date" className="date">
+                  Start Date
+                </label>
+                <DatePicker
+                  onChange={setStartDate}
+                  value={startDate}
+                  clearIcon={null}
+                  format=" dd-MM-y "
+                  maxDate={new Date()}
+                />
+                {error.has('startDate') ? (
+                  <div className="errorMessage">Please enter a valid date</div>
+                ) : (
+                  ''
+                )}
+              </div>
               <Dropdown
-                value={state}
-                className="states"
-                label="State"
-                placeholder="Alabama"
-                options={states.map((state) => state.name)}
+                value={department}
+                className="departments"
+                label="Department"
+                placeholder="Sales"
+                options={departments}
                 onClick={(e: any) => {
-                  setState(e.currentTarget.textContent);
+                  setDepartment(e.currentTarget.textContent);
                 }}
                 children={
-                  error.has('state') ? (
-                    <div className="errorMessage">Please choose a state</div>
+                  error.has('department') ? (
+                    <div className="errorMessage">
+                      Please choose a department
+                    </div>
                   ) : (
                     ''
                   )
                 }
               ></Dropdown>
-              <Input
-                inputName="input-wrapper"
-                labelFor="street"
-                labelText="Street"
-                type="text"
-                id="street"
-                placeholder=" Enter your street"
-                value={street}
-                onChange={(e) => setStreet(e.target.value)}
-              />
-              {error.has('street') ? (
-                <div className="errorMessage">
-                  Please enter at least 2 characters
-                </div>
-              ) : (
-                ''
-              )}
-              <Input
-                inputName="input-wrapper"
-                labelFor="city"
-                labelText="City"
-                type="text"
-                id="city"
-                placeholder=" Enter your city"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-              />
-              {error.has('city') ? (
-                <div className="errorMessage">
-                  Please enter at least 2 characters
-                </div>
-              ) : (
-                ''
-              )}
-              <Input
-                inputName="input-wrapper"
-                labelFor="zipCode"
-                labelText="Zip Code"
-                type="number"
-                id="zipCode"
-                placeholder=" Enter your zip code"
-                value={zipCode}
-                onChange={(e) => setZipCode(e.target.value)}
-              />
-              {error.has('zipCode') ? (
-                <div className="errorMessage">
-                  Please enter at least 5 numbers
-                </div>
-              ) : (
-                ''
-              )}
-            </fieldset>
+            </div>
+            <div className="adress">
+              <fieldset className="adress__fieldset">
+                <legend className="adress__legend">Adress</legend>
+                <Dropdown
+                  value={state.name}
+                  className="states"
+                  label="State"
+                  placeholder="Alabama"
+                  options={states}
+                  onClick={(e: any) => {
+                    setState({
+                      name: e.target.textContent,
+                      value: e.target.nodeName,
+                    });
+                  }}
+                  children={
+                    error.has('state') ? (
+                      <div className="errorMessage">Please choose a state</div>
+                    ) : (
+                      ''
+                    )
+                  }
+                ></Dropdown>
+                <Input
+                  inputName="input-wrapper"
+                  labelFor="street"
+                  labelText="Street"
+                  type="text"
+                  id="street"
+                  placeholder=" Enter your street"
+                  value={street}
+                  onChange={(e) => setStreet(e.target.value)}
+                />
+                {error.has('street') ? (
+                  <div className="errorMessage">
+                    Please enter at least 2 characters
+                  </div>
+                ) : (
+                  ''
+                )}
+                <Input
+                  inputName="input-wrapper"
+                  labelFor="city"
+                  labelText="City"
+                  type="text"
+                  id="city"
+                  placeholder=" Enter your city"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+                {error.has('city') ? (
+                  <div className="errorMessage">
+                    Please enter at least 2 characters
+                  </div>
+                ) : (
+                  ''
+                )}
+                <Input
+                  inputName="input-wrapper"
+                  labelFor="zipCode"
+                  labelText="Zip Code"
+                  type="number"
+                  id="zipCode"
+                  placeholder=" Enter your zip code"
+                  value={zipCode}
+                  onChange={(e) => setZipCode(e.target.value)}
+                />
+                {error.has('zipCode') ? (
+                  <div className="errorMessage">
+                    Please enter at least 5 numbers
+                  </div>
+                ) : (
+                  ''
+                )}
+              </fieldset>
+            </div>
           </div>
-          <Dropdown
-            value={department}
-            className="departments"
-            label="Department"
-            placeholder="Sales"
-            options={departments.map((department) => department.name)}
-            onClick={(e: any) => {
-              setDepartment(e.currentTarget.textContent);
-            }}
-            children={
-              error.has('department') ? (
-                <div className="errorMessage">Please choose a department</div>
-              ) : (
-                ''
-              )
-            }
-          ></Dropdown>
           <button type="submit" className="btn-submit">
             Save
           </button>
