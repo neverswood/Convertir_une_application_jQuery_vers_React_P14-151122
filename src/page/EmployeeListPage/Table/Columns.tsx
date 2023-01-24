152daad15246bfdd5f4e9b4e788694f0e735c230
neverswood <nevers_wood@hotmail.fr>
2023-01-24 21:38:56 +0100

    Change the Columns file for employee table

--- src/page/EmployeeListPage/Table/Columns.tsx
+++ src/page/EmployeeListPage/Table/Columns.tsx
@@ -1,11 +1,65 @@
+import '../Table.scss';
+
 export const COLUMNS = [
-  { Header: 'First Name', accessor: 'first_name' },
-  { Header: 'Last Name', accessor: 'last_name' },
-  { Header: 'Start Date', accessor: 'start_date' },
-  { Header: 'Department', accessor: 'department' },
-  { Header: 'Date of Birth', accessor: 'date_of_birth' },
-  { Header: 'Street', accessor: 'street' },
-  { Header: 'City', accessor: 'city' },
-  { Header: 'State', accessor: 'state' },
-  { Header: 'Zip Code', accessor: 'zip_code' },
+  {
+    name: 'First Name',
+    selector: (row: any) => row.first_name,
+    id: 'first_name',
+    width: '155px',
+    sortable: true,
+  },
+  {
+    name: 'Last Name',
+    selector: (row: any) => row.last_name,
+    id: 'last_name',
+    width: '155px',
+    sortable: true,
+  },
+  {
+    name: 'Start Date',
+    selector: (row: any) => row.start_date,
+    width: '100px',
+    sortable: true,
+  },
+
+  {
+    name: 'Department',
+    selector: (row: any) => row.department,
+    id: 'department',
+    width: '150px',
+    sortable: true,
+  },
+  {
+    name: 'Date of Birth',
+    selector: (row: any) => row.date_of_birth,
+    width: '115px',
+    sortable: true,
+  },
+  {
+    name: 'Street',
+    selector: (row: any) => row.street,
+    id: 'street',
+    width: '196px',
+    sortable: true,
+  },
+  {
+    name: 'City',
+    selector: (row: any) => row.city,
+    width: '200px',
+    sortable: true,
+  },
+  {
+    name: 'State',
+    selector: (row: any) => row.state,
+    id: 'state',
+    width: '196px',
+    sortable: true,
+  },
+  {
+    name: 'Zip Code',
+    selector: (row: any) => row.zip_code,
+    id: 'zip_code',
+    width: '86px',
+    sortable: true,
+  },
 ];
